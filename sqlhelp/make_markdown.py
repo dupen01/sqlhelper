46732b45fe4e0ad7a954fefa0c7804a22d9f4c33
dupeng <dunett@163.com>
2024-07-28 18:42:26 +0800

    1

--- sqlhelp/make_markdown.py
+++ sqlhelp/make_markdown.py
@@ -1,54 +1,40 @@
 from sql_helper import SqlHelper
 
 
-def to_markdown(res: dict):
-    markdown_lst = []
+def _dict_to_mermaid(res: dict):
+    result = []
     target_table = res.get('target_table')
     source_tables = res.get('source_table')
     for table in source_tables:
-        markdown_lst.append(f"{table} --> {target_table}")
-    # if target_table == 'dws_hive':
-    #     print(f'----{res}')
-    return markdown_lst
+        result.append(f"{table} --> {target_table}")
+    return '\n'.join(result)
 
 
-markdown_lst = []
-
-total_sql = []
-
-import glob
-sql_files = glob.glob('sqlhelp/sql-files/*.sql')
-sql_files = glob.glob('sqlhelp/sql-files/query02.sql')
-sql_files = glob.glob('/Users/dupeng/IdeaProjects/duperl/exec-sql/starrocks-sql/dass-backup/*/*.sql')
-# sql_files = glob.glob('/Users/dupeng/IdeaProjects/duperl/exec-sql/starrocks-sql/dass-backup/数字运营_v1_DWS/*.sql')
-# sql_files = glob.glob('/Users/dupeng/IdeaProjects/duperl/exec-sql/starrocks-sql/dass-backup/数字运营_v1_DWS/0_DWS建表语句.sql')
-# sql_files = glob.glob('/Users/dupeng/IdeaProjects/duperl/exec-sql/starrocks-sql/dass-backup/数字运营_v1_DWS/8_体验项目列表.sql')
-# sql_files = glob.glob('/Users/dupeng/IdeaProjects/duperl/exec-sql/starrocks-sql/dass-backup/数字运营_v1_DWS/9_会员体验项目关系.sql')
-
-
-for file in sql_files:
-    with open(file, 'r') as f:
-        sql = f.read()
-        if not sql.strip().endswith(';'):
-            sql = sql + ';'
-        total_sql.append(sql)
+def get_sql_from_file(file_path):
+    import glob
+    total_sql = []
+    files = glob.glob(file_path)
+    for file in files:
+        with open(file, 'r') as f:
+            sql = f.read()
+            if not sql.strip().endswith(';'):
+                sql = sql + ';'
+            total_sql.append(sql)
+    return '\n'.join(total_sql)
 
 
-total_sql_str = '\n'.join(total_sql)
+def sql_to_mermaid(total_sql):
+    total_mermaid = []
+    sql_lst = SqlHelper.split(total_sql)
+    for sql in sql_lst:
+        res = SqlHelper.get_source_target_tables(sql)
+        if res:
+            total_mermaid.append(_dict_to_mermaid(res))
+
+    return '\n'.join(list(set(total_mermaid)))
 
 
-sql_lst = SqlHelper.split(total_sql_str)
-
-for sql in sql_lst:
-    # print(sql)
-    res = SqlHelper.get_source_target_tables(sql)
-    if res:
-        
-        md_lst = to_markdown(res)
-        markdown_lst.extend(md_lst)
-    
-
-markdown_str = '\n'.join(list(set(markdown_lst)))
-markdown_str = 'graph LR\n' + markdown_str
-print(len(list(set(markdown_lst))))
-print(markdown_str)
+if __name__ == '__main__':
+    total_sql = get_sql_from_file('sqlhelp/sql-files/*.sql')
+    mermaid_str = sql_to_mermaid(total_sql)
+    print('graph LR\n' + mermaid_str)
